b2b5b4763e45a3051bdb374001e92de23a92aa19
Michael Urspringer <michael@urspringer.de>
2026-07-20 16:48:10 +0200

    Changed version

--- mc-build.sh
+++ mc-build.sh
@@ -5,7 +5,7 @@ cd "$HOME/meshcore"
 source bin/activate
 cd MeshCore
 
-export FIRMWARE_VERSION="v1.16.0.mu-ufo1BETA"
+export FIRMWARE_VERSION="v1.16.0.mu-ufo1"
 export DISABLE_DEBUG=0
 
 FIRMWARE_BASE="$HOME/meshcore/Firmware-MU"
